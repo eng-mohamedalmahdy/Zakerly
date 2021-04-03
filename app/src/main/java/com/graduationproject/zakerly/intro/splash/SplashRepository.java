2de2ab92099f1778340f42c160d25f549a89639f
mohammed magdy <mjdym7701@gmail.com>
2021-04-03 23:53:02 +0200

    create splash fragment create viewModel class add splash fragment to navigation_app

---
+++ app/src/main/java/com/graduationproject/zakerly/intro/splash/SplashRepository.java
@@ -0,0 +1,12 @@
+package com.graduationproject.zakerly.intro.splash;
+
+import android.content.Context;
+
+public class SplashRepository{
+
+   private Context context;
+
+    public SplashRepository(Context context){
+        this.context = context ;
+    }
+}
---
+++ app/src/main/java/com/graduationproject/zakerly/intro/splash/SplashViewModelFactory.java
@@ -0,0 +1,27 @@
+package com.graduationproject.zakerly.intro.splash;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+
+
+
+public class SplashViewModelFactory  implements ViewModelProvider.Factory{
+
+     private SplashRepository splashRepository ;
+    public SplashViewModelFactory( SplashRepository splashRepository) {
+        this.splashRepository=splashRepository;
+    }
+
+    @NonNull
+    @Override
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) { // create object from viewModel class
+
+        if (modelClass.isAssignableFrom(SplashViewModel.class)) {
+            return (T) new SplashViewModel(splashRepository) ;
+        }
+        throw new IllegalArgumentException("Unable to construct viewmodel") ;
+    }
+}
